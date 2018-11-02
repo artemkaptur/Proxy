d2dcd58b8ade5b3e8bab45eabec684534b9ce4fc
Artsiom Kaptur <Artsiom_Kaptur@epam.com>
2018-11-02 18:00:43 +0300

    Refactor AndroidSimpleTest

--- src/test/java/com/epam/appium/AndroidSimpleTest.java
+++ src/test/java/com/epam/appium/AndroidSimpleTest.java
@@ -1,39 +1,57 @@
 package com.epam.appium;
 
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.concurrent.TimeUnit;
+
+import static io.appium.java_client.touch.offset.PointOption.point;
 
 public class AndroidSimpleTest {
 
-    @Test
-    public void androidSimpleTest() throws MalformedURLException {
+    private static AndroidDriver driver;
+
+    @BeforeClass
+    public void driverSetUp() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName","Android");
-        capabilities.setCapability("platformVersion","4.2.2");
-        capabilities.setCapability("udid", "5a07273f");
-        capabilities.setCapability("deviceName", "Galaxy s4");
+        capabilities.setCapability("platformVersion","6.0.1");
+        capabilities.setCapability("udid", "4d0005c0d4ff91f5");
+        capabilities.setCapability("deviceName", "Galaxy s5");
         capabilities.setCapability("skipUnlock", "true");
         capabilities.setCapability("noReset", "false");
-        capabilities.setCapability("appPackage", "com.android.chrome");
-        capabilities.setCapability("appActivity", "org.chromium.chrome.browser.ChromeTabbedActivity");
+        capabilities.setCapability("appPackage", "com.android.contacts");
+        capabilities.setCapability("appActivity", "com.android.contacts.activities.PeopleActivity");
 
-        AndroidDriver driver = new AndroidDriver(
-                new URL("http://0.0.0.0:4446/wd/hub"), capabilities);
+       driver = new AndroidDriver(new URL("http://0.0.0.0:4446/wd/hub"), capabilities);
+    }
 
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-        driver.navigate().to("https://www.onliner.by/");
+    @Test
+    public void androidSimpleTest() throws MalformedURLException {
+        new TouchAction(driver)
+                .press(point(462, 1541))
+                .moveTo(point(462, 462))
+                .release()
+                .perform();
 
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(driver
+                        .findElement(By.xpath("//android.widget.TextView[@text='Nastassia Suhovei']")))).click();
 
+        Assert.assertTrue(driver.findElement(By.xpath("//android.widget.TextView[@text='+375 29 372-07-93']")).isDisplayed());
+    }
+
+    @AfterClass
+    public void quitDriver()
+    {
         driver.quit();
     }
 
